cbb76ac62850f5acc07e9870272b3641e906c2af
Claudio Jr Familia Morel <claudio.familia.morel@gmail.com>
2022-12-02 17:41:56 -0400

    Update

--- WebApp/src/components/core/Layout/Sidebar/Sidebar.js
+++ WebApp/src/components/core/Layout/Sidebar/Sidebar.js
@@ -2,9 +2,18 @@ import './Sidebar.scss';
 import { NavLink } from 'react-router-dom';
 
 const Sidebar = props => {
+    const usd = 54
+    const eu = 54
     return (
         <div className="srhero__sidebar">
             <ul>
+                <li className='srhero__divisas'>
+                    <h3>Divisas:</h3>
+                    <ul>
+                        <li><span>US:</span> {usd} DOP</li>
+                        <li><span>EU:</span> {eu} DOP</li>
+                    </ul>
+                </li>
                 <li>
                     <NavLink activeClassName='active' to='/users'>Users</NavLink>
                 </li>
